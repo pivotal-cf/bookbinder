e2509a2b0e95f10792dc2fabba9a8c7a840df570
Lucas Marks and Mike Grafton <pair+lmarks+mike@pivotallabs.com>
2013-11-20 18:17:10 -0800

    More spec refactoring

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -12,27 +12,30 @@ describe 'middleman helpers' do
   let(:source_dir) {File.join(tmpdir, 'source')}
 
   describe '#trail_nav' do
+    let(:source_file_content) { '<%= trail_nav %>' }
+
     context 'when invoked in the top-level index file' do
-      before do
-        write_markdown_source_file 'index.md.erb', 'dogs'
-      end
+      let(:source_file_under_test) { 'index.md.erb' }
+      let(:source_file_title) { 'Dogs' }
+      let(:output) { File.read File.join(tmpdir, 'build', 'index.html') }
 
       it 'displays nothing' do
         run_middleman
-        output = File.read File.join(tmpdir, 'build', 'index.html')
         expect(output).to be_empty
       end
     end
 
-    context 'when invoked in an index file in a sub-dir' do
+    context 'when invoked in an index file in a sub-dir, when the parent has a title' do
+      let(:source_file_under_test) { File.join('sub-dir', 'index.md.erb') }
+      let(:source_file_title) { 'Big Dogs' }
+      let(:output) { File.read File.join(tmpdir, 'build', 'sub-dir', 'index.html') }
+
       before do
         write_markdown_source_file 'index.md.erb', 'Dogs'
-        write_markdown_source_file File.join('sub-dir', 'index.md.erb'), 'Big Dogs', '<%= trail_nav %>'
       end
 
       it 'creates a two-level breadcrumb for the two levels of the hierarchy' do
         run_middleman
-        output = File.read File.join(tmpdir, 'build', 'sub-dir', 'index.html')
         doc = Nokogiri::HTML(output)
         expect(doc.at('ul li:first-child').text).to eq('Dogs')
       end
@@ -49,6 +52,7 @@ describe 'middleman helpers' do
 
   def run_middleman
     # awful hacks to eliminate the impact of global state in middleman. when will it end?
+    write_markdown_source_file source_file_under_test, source_file_title, source_file_content
     Middleman::Cli::Build.instance_variable_set(:@_shared_instance, nil)
     ENV["MM_ROOT"] = tmpdir
 
@@ -57,5 +61,4 @@ describe 'middleman helpers' do
       build_command.invoke :build, [], {'instrument' => 'false'}
     end
   end
-
 end
