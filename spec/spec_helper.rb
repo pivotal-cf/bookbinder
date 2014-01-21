7dfc1e34424a6029cff60d27dcd52a0c17b31036
Gavin Morgan and Nikhil Gajwani <pair+gavin+ngajwani@pivotallabs.com>
2014-01-20 16:16:38 -0800

    Set ENV in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,6 +32,9 @@ RSpec.configure do |config|
   config.before do
     # awful hack to prevent tests that invoke middleman directly from polluting code that shells out to call it
     ENV['MM_ROOT'] = nil
+
+    #the Github API token must be in the ENV or tests will fail.
+    ENV['GITHUB_API_TOKEN'] = 'foo'
   end
 
   config.before do
