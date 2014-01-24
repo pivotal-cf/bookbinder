fba210b974cf55b30e1526705ff2fd55561a803e
Gavin Morgan and Nikhil Gajwani <pair+gavin+ngajwani@pivotallabs.com>
2014-01-24 10:49:29 -0800

    Refactors test to not require fixture config.yml syncing.

--- spec/commands/tag_spec.rb
+++ spec/commands/tag_spec.rb
@@ -26,8 +26,6 @@ describe Cli::Tag do
     FileUtils.cd(book_dir) { spec.run }
   end
 
-  # Repos are copied from fixture data, and may fall out of sync
-  let(:repos) { [{"github_repo"=>"fantastic/dogs-repo", "directory"=>"dogs"}, {"github_repo"=>"fantastic/my-docs-repo", "directory"=>"docs"}, {"github_repo"=>"fantastic/my-other-docs-repo", "directory"=>"others"}] }
   let(:book_name) { 'wow-org/such-book' }
   let(:book_sha) { 26.times.map { (65 + rand(26)).chr }.join  }
   let(:desired_tag) { 12.times.map { (65 + rand(26)).chr }.join  }
@@ -36,8 +34,8 @@ describe Cli::Tag do
     Octokit::Client.any_instance.stub(:octocat).and_return 'truthy kitten string'
     Octokit::Client.any_instance.stub(:commits).and_return [OpenStruct.new(sha: 'some-sha')]
     Octokit::Client.any_instance.stub(:create_ref).and_return 'something truthy'
-    @book = Book.new(full_name: book_name, constituent_params: repos)
-    Book.stub(:new).with(full_name: book_name, constituent_params: repos).and_return(@book)
+    @book = Book.new(full_name: book_name)
+    Book.stub(:new).with { |args| args[:full_name].should eq book_name }.and_return(@book)
     DocRepo.any_instance.stub(:tag_with)
   end
 
