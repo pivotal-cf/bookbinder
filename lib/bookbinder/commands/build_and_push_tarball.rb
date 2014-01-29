5e1a179988b5451545f929cfd95c50e2787ddedf
Gavin Morgan and Nikhil Gajwani <pair+gavin+ngajwani@pivotallabs.com>
2014-01-29 14:51:08 -0800

    Notify user if AWS key is needed, but missing from config.yml.

--- lib/bookbinder/commands/build_and_push_tarball.rb
+++ lib/bookbinder/commands/build_and_push_tarball.rb
@@ -1,8 +1,8 @@
 class Cli
   class BuildAndPushTarball < BookbinderCommand
     def run(_)
-      repository = GreenBuildRepository.new key: config['aws']['access_key'],
-                                            secret: config['aws']['secret_key']
+      repository = GreenBuildRepository.new key: config.fetch('aws')['access_key'],
+                                            secret: config.fetch('aws')['secret_key']
 
       raise 'You must set $BUILD_NUMBER to push an identifiable build.' unless ENV['BUILD_NUMBER']
       repository.create build_number: ENV['BUILD_NUMBER'], bucket: config['aws']['green_builds_bucket']
