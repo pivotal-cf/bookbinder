2903976da806d184d58df21088530fc5d0e2fa36
Gavin Morgan and Nikhil Gajwani <pair+gavin+ngajwani@pivotallabs.com>
2014-01-30 11:50:51 -0800

    Refactor test to ensure repos are only tagged once.
    This was never a bug, but now the test will catch it if it becomes so.

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,25 +1,24 @@
 require 'spec_helper'
 
 describe Book do
-  let(:repos) do
-    [{"github_repo" => "fantastic/dogs-repo", "directory" => "dogs"},
-     {"github_repo" => "fantastic/my-docs-repo", "directory" => "docs"},
-     {"github_repo" => "fantastic/my-other-docs-repo", "directory" => "others"}]
+  let(:constituents) do
+    [{"github_repo" => "fantastic/dogs-repo", "directory" => "dogs"}]
   end
+
   let(:book_name) { 'wow-org/such-book' }
 
   before do
     @constituent = double
-    DocRepo.stub(:from_remote).and_return(@constituent)
-    @book = Book.new(full_name: book_name, constituent_params: repos)
-    Book.stub(:new).with(full_name: book_name, constituent_params: repos).and_return(@book)
+    DocRepo.stub(:from_remote).and_return @constituent
+    @book = Book.new full_name: book_name, constituent_params: constituents
+    Book.stub(:new).with(full_name: book_name, constituent_params: constituents).and_return @book
   end
 
   describe '#tag_constituents' do
     let(:desired_tag) { 12.times.map { (65 + rand(26)).chr }.join  }
 
     it 'should tag the constituent repos' do
-      @constituent.should_receive(:tag_with).with(desired_tag).at_least(1).times
+      @constituent.should_receive(:tag_with).with desired_tag
       @book.tag_constituents_with(desired_tag)
     end
   end
