1ed5e903a45fd5aa7120508115f361dbed1bf3ae
Gavin Morgan and Nikhil Gajwani <pair+gavin+ngajwani@pivotallabs.com>
2014-02-21 12:24:02 -0800

    PushToProd cleanup

--- lib/bookbinder/commands/push_to_prod.rb
+++ lib/bookbinder/commands/push_to_prod.rb
@@ -2,21 +2,24 @@ class Cli
   class PushToProd < BookbinderCommand
     def run(arguments)
       app_dir = Dir.mktmpdir
-      repository = GreenBuildRepository.new key: config.fetch('aws').fetch('access_key'),
-                                            secret: config.fetch('aws').fetch('secret_key')
+
+      aws_params = config.fetch('aws')
+      repository = GreenBuildRepository.new key: aws_params.fetch('access_key'),
+                                            secret: aws_params.fetch('secret_key')
 
       repository.download download_dir: app_dir,
-                          bucket: config.fetch('aws').fetch('green_builds_bucket'),
+                          bucket: aws_params.fetch('green_builds_bucket'),
                           build_number: arguments[0],
                           namespace: Book.new(full_name: config.fetch('github_repo')).short_name
 
       log 'Warning: You are pushing to CF Docs production. Be careful.'.yellow
 
-      Pusher.new.push config.fetch('cloud_foundry').fetch('api_endpoint'),
-                      config.fetch('cloud_foundry').fetch('production_host'),
-                      config.fetch('cloud_foundry').fetch('organization'),
-                      config.fetch('cloud_foundry').fetch('production_space'),
-                      config.fetch('cloud_foundry').fetch('app_name'),
+      cf_params = config.fetch('cloud_foundry')
+      Pusher.new.push cf_params.fetch('api_endpoint'),
+                      cf_params.fetch('production_host'),
+                      cf_params.fetch('organization'),
+                      cf_params.fetch('production_space'),
+                      cf_params.fetch('app_name'),
                       app_dir
 
       0
