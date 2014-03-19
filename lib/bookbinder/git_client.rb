bb54b53a134ea387a28488132921243269311198
Dan Wendorf <github@danwendorf.com>
2014-03-18 17:33:11 -0700

    Creating a tag also adds a message
    [finishes #65032326]

--- lib/bookbinder/git_client.rb
+++ lib/bookbinder/git_client.rb
@@ -1,5 +1,6 @@
 class GitClient < Octokit::Client
-  class GitClient::TokenException < StandardError;end
+  class GitClient::TokenException < StandardError;
+  end
 
   def self.get_instance(*args)
     @@shared_instance ||= new(*args)
@@ -12,7 +13,8 @@ class GitClient < Octokit::Client
   end
 
   def create_tag!(full_name, tagname, ref)
-    self.create_ref(full_name, "tags/#{tagname}", ref)
+    create_ref(full_name, "tags/#{tagname}", ref)
+    create_tag(full_name, "tags/#{tagname}", 'Tagged by Bookbinder', ref, 'commit', 'Bookbinder', 'bookbinder@cloudfoundry.org', Time.now.iso8601)
   rescue Octokit::Unauthorized, Octokit::NotFound
     raise_error_with_context
   end
@@ -26,7 +28,7 @@ class GitClient < Octokit::Client
   private
 
   def raise_error_with_context
-    absent_token_message  = '  Cannot access repository! You must set $GITHUB_API_TOKEN.'
+    absent_token_message = '  Cannot access repository! You must set $GITHUB_API_TOKEN.'
     invalid_token_message = '  Cannot access repository! Does your $GITHUB_API_TOKEN have access to this repository? Does it exist?'
 
     ENV['GITHUB_API_TOKEN'] ? raise(TokenException.new(invalid_token_message)) : raise(TokenException.new(absent_token_message))
--- spec/git_client_spec.rb
+++ spec/git_client_spec.rb
@@ -3,14 +3,27 @@ require 'spec_helper'
 describe GitClient do
   describe 'create_tag!' do
     let(:tagname) { 'sofia-1.0.1' }
-    let(:full_name) { 'foo/bar' }
+    let(:repo_name) { 'foo/bar' }
     let(:sha) { 'some-sha' }
+    let(:client) { GitClient.get_instance }
+    let(:iso_time) { '2014-03-18T17:26:38-07:00' }
 
-    before { Octokit::Client.any_instance.stub(:commits) }
+    before do
+      allow(Time).to receive(:now).and_return(double(:time, iso8601: iso_time))
+    end
+
+    it 'passes the right refname to OctoKit' do
+      allow(client).to receive(:create_tag)
+
+      expect(client).to receive(:create_ref).with(repo_name, "tags/#{tagname}", sha)
+      client.create_tag!(repo_name, tagname, sha)
+    end
 
     it 'passes the right tag to OctoKit' do
-      Octokit::Client.any_instance.should_receive(:create_ref).with(full_name, "tags/#{tagname}", sha)
-      GitClient.get_instance.create_tag!(full_name, tagname, sha)
+      allow(client).to receive(:create_ref)
+
+      expect(client).to receive(:create_tag).with(repo_name, "tags/#{tagname}", 'Tagged by Bookbinder', sha, 'commit', 'Bookbinder', 'bookbinder@cloudfoundry.org', iso_time)
+      client.create_tag!(repo_name, tagname, sha)
     end
   end
 end
