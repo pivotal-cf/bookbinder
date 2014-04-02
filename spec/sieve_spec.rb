59387d88f3181ce93a40dcc70eeeeaa59e43192c
Elana Koren <ekoren@pivotallabs.com>
2014-04-02 16:03:24 -0700

    Sieve spec uses modern RSpec style
    Signed-off-by: Dan Wendorf <github@danwendorf.com>

--- spec/sieve_spec.rb
+++ spec/sieve_spec.rb
@@ -3,46 +3,49 @@ require 'spec_helper'
 describe Sieve do
   describe '#links_from' do
     let(:sieve) { Sieve.new(domain: root_page_url) }
-    let(:page) { double }
+    let(:page) { double(localized_links_in_stylesheets: []) }
     let(:root_page_url) { 'http://example.com/' }
 
     context 'when the page is found' do
       before do
-        page.stub(:not_found?) { false }
-        page.stub(:url) { root_page_url }
-        page.stub(:fragment_identifiers) { frags }
-        page.stub(:localized_links_in_stylesheets) {[]}
+        allow(page).to receive(:not_found?).and_return(false)
+        allow(page).to receive(:url).and_return(root_page_url)
       end
 
       context 'and it has a valid html body' do
         let(:frags) { [URI('#this-is-broken')] }
 
         before do
-          page.stub(:has_target_for?).with(frags.first).and_return false
+          allow(page).to receive(:fragment_identifiers).and_return(frags)
+          allow(page).to receive(:has_target_for?).with(frags.first).and_return(false)
         end
 
         it 'returns the arrays passed to it, filled with links from the given page' do
-          broken_links, working_links = sieve.links_from page, true
+          broken_links, working_links = sieve.links_from(page, true)
 
-          broken_links.should =~ ["/ => #{frags.pop}"]
-          working_links.should =~ [root_page_url]
+          expect(broken_links).to eq(["/ => #{frags.pop}"])
+          expect(working_links).to eq([root_page_url])
         end
+      end
+
+      context 'and it does not have a valid html body' do
+        pending
       end
     end
 
     context 'when the page is not found' do
       before do
-        page.stub(:not_found?).and_return { true }
-        page.stub(:url).and_return { "#{root_page_url}fake" }
-        page.stub(:referer).and_return { root_page_url }
+        allow(page).to receive(:not_found?).and_return(true)
+        allow(page).to receive(:url).and_return("#{root_page_url}fake")
+        allow(page).to receive(:referer).and_return(root_page_url)
       end
 
       it 'returns the arrays passed to it, filled with links from the given page' do
         is_second_pass = true
-        broken_links, working_links = sieve.links_from page, is_second_pass
+        broken_links, working_links = sieve.links_from(page, is_second_pass)
 
-        broken_links.should =~ ['/ => http://example.com/fake']
-        working_links.should =~ []
+        expect(broken_links).to eq(['/ => http://example.com/fake'])
+        expect(working_links).to eq([])
       end
     end
   end
