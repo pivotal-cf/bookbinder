3895c258a094aa98e427086164605bb0799be8e9
Matthew Boedicker <mboedicker@pivotallabs.com>
2014-04-16 13:50:03 -0700

    Consolidate like specs
    Signed-off-by: Gavin Morgan <gavin@pivotallabs.com>

--- spec/integration/publishing_a_book_spec.rb
+++ spec/integration/publishing_a_book_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'calling bookbinder with pdf configuration' do
+describe 'publishing a book' do
   include_context 'tmp_dirs'
 
   around_with_fixture_repo do |spec|
@@ -11,6 +11,15 @@ describe 'calling bookbinder with pdf configuration' do
     config = YAML.load(File.read('./config.yml'))
     config.delete('cred_repo')
     File.write('./config.yml', config.to_yaml)
+  end
+
+  it 'provides the production host to the ERB templates' do
+    silence_io_streams do
+      `#{GEM_ROOT}/bin/bookbinder publish local`
+    end
+
+    index = File.read File.join('final_app', 'public', 'index.html')
+    expect(index).to include('My production host is: docs.example.com')
   end
 
   it 'generates a pdf based on the filename option' do
