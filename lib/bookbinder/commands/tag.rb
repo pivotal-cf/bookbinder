83cb11edc8f190c26abe33c87c645bcf8f4a980a
Gavin Morgan <gavin@pivotallabs.com>
2014-05-14 15:58:25 -0700

    Improve user output when tagging.
    Signed-off-by: Matthew Boedicker <mboedicker@pivotallabs.com>

--- lib/bookbinder/commands/tag.rb
+++ lib/bookbinder/commands/tag.rb
@@ -10,7 +10,7 @@ module Bookbinder
         book.tag_self_and_sections_with tag
 
         @logger.log 'Success!'.green
-        @logger.log " #{book.full_name.yellow} its document repositories were tagged with #{tag.blue}"
+        @logger.log " #{book.full_name.yellow} and its sections were tagged with #{tag.blue}"
         0
       end
 
