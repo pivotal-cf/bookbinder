bb9832d78c30cfb6e27a012bcd0edf9357504887
Andrew Bruce <andrew@camelpunch.com>
2015-01-01 15:27:43 +0000

    More descriptive spec for pusher
    [#83972842]

--- spec/pusher_spec.rb
+++ spec/pusher_spec.rb
@@ -6,11 +6,11 @@ module Bookbinder
 
     let(:creds) do
       Configuration::CfCredentials.new(
-          {
-              'api_endpoint' => nil,
-              'app_name' => 'app'
-          },
-          false
+        {
+          'api_endpoint' => nil,
+          'app_name' => 'app'
+        },
+        false
       )
     end
 
@@ -21,37 +21,35 @@ module Bookbinder
     subject(:pusher) { Pusher.new(cf) }
     let(:app_dir) { tmp_subdir "pusher_spec" }
 
-    describe '#push' do
-      describe 'when the hostname points to green' do
-        before do
-          expect(cf).to receive(:login).with(no_args).ordered
-          expect(cf).to receive(:apps).with(no_args).and_return([['app-green']]).ordered
-        end
-
-        it 'makes the right CF calls' do
-          expect(cf).to receive(:start).with('app-blue').ordered
-          expect(cf).to receive(:push).with('app-blue').ordered
-          expect(cf).to receive(:map_routes).with('app-blue').ordered
-          expect(cf).to receive(:takedown_old_target_app).with('app-green').ordered
-
-          pusher.push(app_dir)
-        end
+    describe 'when the hostname points to green' do
+      before do
+        expect(cf).to receive(:login).with(no_args).ordered
+        expect(cf).to receive(:apps).with(no_args).and_return([['app-green']]).ordered
       end
 
-      describe 'when the hostname points to blue' do
-        before do
-          expect(cf).to receive(:login).with(no_args).ordered
-          expect(cf).to receive(:apps).with(no_args).and_return([['app-blue']]).ordered
-        end
+      it 'starts the blue app, then remaps before taking the green app down' do
+        expect(cf).to receive(:start).with('app-blue').ordered
+        expect(cf).to receive(:push).with('app-blue').ordered
+        expect(cf).to receive(:map_routes).with('app-blue').ordered
+        expect(cf).to receive(:takedown_old_target_app).with('app-green').ordered
 
-        it 'makes the right CF calls' do
-          expect(cf).to receive(:start).with('app-green').ordered
-          expect(cf).to receive(:push).with('app-green').ordered
-          expect(cf).to receive(:map_routes).with('app-green').ordered
-          expect(cf).to receive(:takedown_old_target_app).with('app-blue').ordered
+        pusher.push(app_dir)
+      end
+    end
 
-          pusher.push(app_dir)
-        end
+    describe 'when the hostname points to blue' do
+      before do
+        expect(cf).to receive(:login).with(no_args).ordered
+        expect(cf).to receive(:apps).with(no_args).and_return([['app-blue']]).ordered
+      end
+
+      it 'starts the green app, then remaps before taking the blue app down' do
+        expect(cf).to receive(:start).with('app-green').ordered
+        expect(cf).to receive(:push).with('app-green').ordered
+        expect(cf).to receive(:map_routes).with('app-green').ordered
+        expect(cf).to receive(:takedown_old_target_app).with('app-blue').ordered
+
+        pusher.push(app_dir)
       end
     end
   end
