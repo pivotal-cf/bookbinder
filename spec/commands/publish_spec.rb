ab016cdfa5cc6520e530f290ce081a5b1288dbdb
Frank Kotsianas <frankkotsianas@gmail.com>
2015-01-16 10:39:42 -0800

    move publisher integration tests to publish
    Signed-off-by: Eric Tsiliacos <etsiliacos@pivotal.io>

--- spec/commands/publish_spec.rb
+++ spec/commands/publish_spec.rb
@@ -104,6 +104,14 @@ module Bookbinder
         }.from(Net::HTTPSuccess).to(Net::HTTPMovedPermanently)
       end
 
+      it 'it can find repos locally rather than going to github' do
+        final_app_dir = File.absolute_path('final_app')
+        publish_command.run(['local'], SpecGitAccessor)
+
+        index_html = File.read File.join(final_app_dir, 'public', 'foods/sweet', 'index.html')
+        expect(index_html).to include 'This is a Markdown Page'
+      end
+
       context 'when provided a layout repo' do
         let(:config_hash) do
           {'sections' => sections, 'book_repo' => book, 'pdf_index' => [], 'public_host' => 'example.com', 'layout_repo' => 'such-org/layout-repo'}
@@ -119,6 +127,50 @@ module Bookbinder
           publish_command.run(['local'], SpecGitAccessor)
         end
       end
+
+      context 'when code snippets are yielded' do
+        let(:non_broken_master_middleman_dir) { generate_middleman_with 'remote_code_snippets_index.html' }
+
+        context 'and the code repo is present' do
+          it 'can find code example repos locally rather than going to github' do
+            expect(SpecGitAccessor).to_not receive(:clone)
+
+            publish_command.run(['local'], SpecGitAccessor)
+          end
+        end
+
+        context 'and the code repo is absent' do
+          it 'fails out' do
+            sections = [
+                    {'repository' => {
+                        'name' => 'dev/null',
+                        'ref' => 'dog-sha'},
+                     'directory' => 'dogs',
+                     'subnav_template' => 'dogs'}
+
+            ]
+
+            config_hash = {
+                'sections' => sections,
+                'book_repo' => book,
+                'pdf_index' => [],
+                'public_host' => 'example.com',
+            }
+
+            config = Configuration.new(logger, config_hash)
+
+            configuration_fetcher = double('configuration_fetcher')
+            allow(configuration_fetcher).to receive(:fetch_config).and_return(config)
+
+            publish_command = Commands::Publish.new(logger, configuration_fetcher)
+
+            allow(logger).to receive(:log)
+            expect(logger).to receive(:log).with /skipping \(not found\)/
+            publish_command.run(['local'], SpecGitAccessor)
+            expect(WebMock).not_to have_requested(:get, 'https://api.github.com/repos/fantastic/code-example-repo/tarball/master')
+          end
+        end
+      end
     end
 
     describe 'github' do
@@ -129,6 +181,35 @@ module Bookbinder
 
         index_html = File.read File.join('final_app', 'public', 'foods', 'sweet', 'index.html')
         expect(index_html).to include 'This is a Markdown Page'
+      end
+
+      it 'creates a directory per repo with the generated html from middleman' do
+        expect(SpecGitAccessor).to receive(:clone).with("git@github.com:#{'fantastic/dogs-repo'}",
+                                                        "dogs",
+                                                        anything).and_call_original
+
+        expect(SpecGitAccessor).to receive(:clone).with("git@github.com:#{'fantastic/my-docs-repo'}",
+                                                        "foods/sweet",
+                                                        anything).and_call_original
+
+        expect(SpecGitAccessor).to receive(:clone).with("git@github.com:#{'fantastic/my-other-docs-repo'}",
+                                                        "foods/savory",
+                                                        anything).and_call_original
+
+        silence_io_streams do
+          publish_command.run(['github'], SpecGitAccessor)
+        end
+
+        final_app_dir = File.absolute_path('final_app')
+
+        index_html = File.read File.join(final_app_dir, 'public', 'dogs', 'index.html')
+        expect(index_html).to include 'breeds.png'
+
+        other_index_html = File.read File.join(final_app_dir, 'public', 'foods/sweet', 'index.html')
+        expect(other_index_html).to include 'This is a Markdown Page'
+
+        third_index_html = File.read File.join(final_app_dir, 'public', 'foods/savory', 'index.html')
+        expect(third_index_html).to include 'This is another Markdown Page'
       end
 
       context 'when provided a layout repo' do
@@ -243,6 +324,235 @@ module Bookbinder
         expect {
           publish_command.run([], SpecGitAccessor)
         }.to raise_error(CliError::InvalidArguments)
+      end
+    end
+
+    describe 'generating links' do
+      it 'succeeds when all links are functional' do
+        no_broken_links = publish_command.run(['github'], SpecGitAccessor)
+
+        expect(no_broken_links).to eq 0
+      end
+    end
+
+    describe 'using template variables' do
+      it 'includes them into middleman' do
+        sections = [
+            {'repository' => {
+                'name' => 'fantastic/my-variable-repo'},
+             'directory' => 'var-repo'
+            }
+        ]
+
+        config_hash = {
+            'sections' => sections,
+            'book_repo' => book,
+            'pdf_index' => [],
+            'public_host' => 'example.com',
+            'template_variables' => { 'name' => 'Spartacus'}
+        }
+
+        config = Configuration.new(logger, config_hash)
+        configuration_fetcher = double('configuration_fetcher')
+        allow(configuration_fetcher).to receive(:fetch_config).and_return(config)
+
+        publish_command = Commands::Publish.new(logger, configuration_fetcher)
+        publish_command.run(['github'], SpecGitAccessor)
+
+        final_app_dir = File.absolute_path('final_app')
+        index_html = File.read File.join(final_app_dir, 'public', 'var-repo', 'variable_index.html')
+        expect(index_html).to include 'My variable name is Spartacus.'
+      end
+    end
+
+    describe 'including code snippets' do
+      it 'applies the syntax highlighting CSS' do
+        section_repo_name = 'org/my-repo-with-code-snippets'
+        code_repo = 'cloudfoundry/code-example-repo'
+
+        expect(SpecGitAccessor).to receive(:clone).with("git@github.com:#{section_repo_name}",
+                                                        'my-code-snippet-repo',
+                                                        anything).and_call_original
+        expect(SpecGitAccessor).to receive(:clone).with("git@github.com:#{code_repo}",
+                                                        'code-example-repo',
+                                                        anything).and_call_original
+
+        sections = [
+            {'repository' => {
+                'name' => 'org/my-repo-with-code-snippets'},
+             'directory' => 'my-code-snippet-repo'
+            }
+        ]
+
+        config_hash = {
+            'sections' => sections,
+            'book_repo' => book,
+            'pdf_index' => [],
+            'public_host' => 'example.com',
+        }
+
+        config = Configuration.new(logger, config_hash)
+        configuration_fetcher = double('configuration_fetcher')
+        allow(configuration_fetcher).to receive(:fetch_config).and_return(config)
+
+        publish_command = Commands::Publish.new(logger, configuration_fetcher)
+        silence_io_streams do
+          publish_command.run(['github'], SpecGitAccessor)
+        end
+
+        final_app_dir = File.absolute_path('final_app')
+        index_html = File.read(File.join(final_app_dir, 'public', 'my-code-snippet-repo', 'code_snippet_index.html'))
+        doc = Nokogiri::HTML(index_html)
+
+        ruby_part = 'fib = Enumerator.new do |yielder|'
+        yaml_part = 'this_is_yaml'
+        typeless_part = 'this = untyped_code'
+
+        ruby_text = doc.css('.highlight.ruby').text
+        expect(ruby_text).to include(ruby_part)
+        expect(ruby_text).not_to include(yaml_part)
+        expect(ruby_text).not_to include(typeless_part)
+
+        yaml_text = doc.css('.highlight.yaml').text
+        expect(yaml_text).to include(yaml_part)
+        expect(yaml_text).not_to include(ruby_part)
+        expect(yaml_text).not_to include(typeless_part)
+
+        typeless_text = doc.css('.highlight.plaintext').text
+        expect(typeless_text).to include(typeless_part)
+        expect(typeless_text).not_to include(yaml_part)
+        expect(typeless_text).not_to include(ruby_part)
+      end
+
+      it 'makes only one request per code example repository' do
+        Repositories::SectionRepository::SHARED_CACHE.clear
+        section_repo_name = 'org/my-repo-with-code-snippets'
+        code_repo = 'cloudfoundry/code-example-repo'
+
+        expect(SpecGitAccessor).to receive(:clone).with("git@github.com:#{section_repo_name}",
+                                                        'my-code-snippet-repo',
+                                                        anything).and_call_original
+        expect(SpecGitAccessor).to receive(:clone).with("git@github.com:#{code_repo}",
+                                                        'code-example-repo',
+                                                        anything).and_call_original
+
+        sections = [
+            {'repository' => {
+                'name' => 'org/my-repo-with-code-snippets'},
+             'directory' => 'my-code-snippet-repo'
+            }
+        ]
+
+        config_hash = {
+            'sections' => sections,
+            'book_repo' => book,
+            'pdf_index' => [],
+            'public_host' => 'example.com',
+        }
+
+        config = Configuration.new(logger, config_hash)
+        configuration_fetcher = double('configuration_fetcher')
+        allow(configuration_fetcher).to receive(:fetch_config).and_return(config)
+
+        publish_command = Commands::Publish.new(logger, configuration_fetcher)
+        silence_io_streams do
+          publish_command.run(['github', '--verbose'], SpecGitAccessor)
+        end
+      end
+    end
+
+    describe 'generates a site-map' do
+      context 'when the hostname is not a single string' do
+        it 'raises' do
+          expect do
+            sections = [
+                { 'repository' => {'name' => 'org/dogs-repo'} }
+            ]
+
+            config_hash = {
+                'sections' => sections,
+                'book_repo' => book,
+                'pdf_index' => [],
+                'public_host' => ['host1.runpivotal.com', 'host2.pivotal.io'],
+            }
+
+            config = Configuration.new(logger, config_hash)
+            configuration_fetcher = double('configuration_fetcher')
+            allow(configuration_fetcher).to receive(:fetch_config).and_return(config)
+
+            publish_command = Commands::Publish.new(logger, configuration_fetcher)
+            publish_command.run(['github'], SpecGitAccessor)
+          end.to raise_error "Your public host must be a single String."
+        end
+      end
+
+      context 'when the hostname is a single string' do
+        around do |spec|
+          WebMock.disable_net_connect!(:allow_localhost => true)
+          spec.run
+          WebMock.disable_net_connect!
+        end
+
+        it 'contains the given pages in an XML sitemap' do
+          book_dir = File.absolute_path('.')
+          middleman_source_dir = File.join(book_dir, 'master_middleman', 'source')
+          FileUtils.mkdir_p middleman_source_dir
+          FileUtils.cp File.expand_path('../../fixtures/dogs_index.html', __FILE__), File.join(middleman_source_dir, 'index.html.md.erb')
+
+          sections = [
+              { 'repository' => {'name' => 'org/dogs-repo'} }
+          ]
+
+          config_hash = {
+              'sections' => sections,
+              'book_repo' => book,
+              'public_host' => 'docs.dogs.com'
+          }
+
+          config = Configuration.new(logger, config_hash)
+          configuration_fetcher = double('configuration_fetcher')
+          allow(configuration_fetcher).to receive(:fetch_config).and_return(config)
+
+          publish_command = Commands::Publish.new(logger, configuration_fetcher)
+          publish_command.run(['github'], SpecGitAccessor)
+
+          final_app_dir = File.absolute_path('final_app')
+          doc = Nokogiri::XML(File.open File.join(final_app_dir, 'public', 'sitemap.xml'))
+          expect(doc.css('loc').map &:text).to match_array(%w(
+                http://docs.dogs.com/index.html
+                http://docs.dogs.com/dogs-repo/index.html
+                http://docs.dogs.com/dogs-repo/big_dogs/index.html
+                http://docs.dogs.com/dogs-repo/big_dogs/great_danes/index.html
+              ))
+        end
+      end
+    end
+
+    describe 'creating subdirectories for a section with a multileveled output directory' do
+      it 'creates intermediate directories' do
+        sections = [
+            {
+                'repository' => { 'name' => 'my-docs-org/my-docs-repo', 'ref' => 'some-sha' },
+                'directory' => 'a/b/c'
+            }
+        ]
+
+        config_hash = {
+            'sections' => sections,
+            'book_repo' => book,
+            'public_host' => 'docs.dogs.com'
+        }
+
+        config = Configuration.new(logger, config_hash)
+        configuration_fetcher = double('configuration_fetcher')
+        allow(configuration_fetcher).to receive(:fetch_config).and_return(config)
+
+        publish_command = Commands::Publish.new(logger, configuration_fetcher)
+        publish_command.run(['github'], SpecGitAccessor)
+
+        final_app_dir = File.absolute_path('final_app')
+        index_html = File.read(File.join(final_app_dir, 'public', 'a', 'b', 'c', 'index.html'))
+        expect(index_html).to include('This is a Markdown Page')
       end
     end
 
