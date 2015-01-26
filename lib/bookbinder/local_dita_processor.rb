643dcad0cc54c75ee56f32447d54b532c212268b
Eric Tsiliacos <etsiliacos@pivotal.io>
2015-01-26 10:19:17 -0800

    use ruby 2.0.0 syntax for named arguments in another place
    Signed-off-by: Frank Kotsianas <frankkotsianas@gmail.com>

--- lib/bookbinder/local_dita_processor.rb
+++ lib/bookbinder/local_dita_processor.rb
@@ -7,7 +7,7 @@ module Bookbinder
       @path_to_dita_ant_script = path_to_dita_ant_script
     end
 
-    def process(dita_sections, to: )
+    def process(dita_sections, to: nil)
       dita_sections.map do |dita_section|
         absolute_path_to_ditamap = File.join dita_section.path_to_local_repo, dita_section.ditamap_location
         out_dir = File.join to, dita_section.directory
