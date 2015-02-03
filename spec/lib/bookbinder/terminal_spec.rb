6a57dbbd04ca26bfdab1f64173aa194beef476af
Frank Kotsianas <frankkotsianas@gmail.com>
2015-02-03 11:48:49 -0800

    fix english test language to match tests in terminal spec
    Signed-off-by: Eric Tsiliacos <etsiliacos@pivotal.io>

--- spec/lib/bookbinder/terminal_spec.rb
+++ spec/lib/bookbinder/terminal_spec.rb
@@ -1,26 +1,23 @@
 require_relative '../../../lib/bookbinder/terminal'
-require_relative '../../../lib/bookbinder/command_validator'
 
 module Bookbinder
   describe Terminal do
-    context 'when the escalation type is of type error' do
-      it 'updates the user interface with a message in red color' do
-        terminal = Terminal.new
+    it 'updates the user interface with a message' do
+      terminal = Terminal.new
 
-        begin
-          real_stdout = $stdout
-          $stdout = StringIO.new
+      begin
+        real_stdout = $stdout
+        $stdout = StringIO.new
 
-          terminal.update('user_message')
+        terminal.update('user_message')
 
-          $stdout.rewind
-          collected_output = $stdout.read
+        $stdout.rewind
+        collected_output = $stdout.read
 
-          expect(collected_output).to eq("user_message\n")
+        expect(collected_output).to eq("user_message\n")
 
-        ensure
-          $stdout = real_stdout
-        end
+      ensure
+        $stdout = real_stdout
       end
     end
   end
