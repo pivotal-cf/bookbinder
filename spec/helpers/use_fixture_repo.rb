9504e8f95bb1917b3157a30033881ac936d8ecc0
Andrew Bruce <andrew@camelpunch.com>
2015-02-03 23:00:49 +0000

    use_fixture_repo takes optional repo name
    [#79052198]

--- spec/helpers/use_fixture_repo.rb
+++ spec/helpers/use_fixture_repo.rb
@@ -1,10 +1,10 @@
 require_relative '../fixtures/repo_fixture'
 
-def use_fixture_repo
+def use_fixture_repo(repo_name = 'book')
   around do |spec|
     temp_library = tmp_subdir 'repositories'
     FileUtils.cp_r File.join(Bookbinder::RepoFixture.repos_dir, '.'), temp_library
-    FileUtils.cd(File.join(temp_library, 'book')) do
+    FileUtils.cd(File.join(temp_library, repo_name)) do
       spec.run
     end
   end
