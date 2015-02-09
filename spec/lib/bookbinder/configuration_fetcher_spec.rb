9a3edf4671e1cbb220f6c1cd037cfa1270aa132e
Andrew Bruce <andrew@camelpunch.com>
2015-02-09 16:49:44 +0000

    Move lets into local variables
    They were only ever used in one example, and were muddying the context
    of the other examples.
    
    Also, fix ands to be whens
    
    [#79052198]

--- spec/lib/bookbinder/configuration_fetcher_spec.rb
+++ spec/lib/bookbinder/configuration_fetcher_spec.rb
@@ -13,56 +13,31 @@ module Bookbinder
     end
 
     describe 'fetching the config' do
-      let(:section1) do
-        {
-          'repository' => {
-            'name' => 'foo/dogs-repo'
-          },
-          'directory' => 'concepts'
-        }
-      end
-      let(:config_hash_in_file) do
-        {
-          'sections' => [section1],
-          'public_host' => 'http://example.com',
-        }
-      end
-      let(:expected_config_hash) do
-        {
-          'sections' => [section1],
-          'public_host' => 'http://example.com',
-          'pdf_index' => nil
-        }
-      end
-
       before do
         allow(config_validator).to receive(:valid?).and_return true
       end
 
-      context 'and the configuration file does not exist' do
-        it 'should raise an informative error' do
-          allow(loader).to receive(:load).with(path_to_config_file).
-            and_raise FileNotFoundError, "YAML"
-          expect { config_fetcher.fetch_config }.to raise_error /The configuration file specified does not exist. Please create a config YAML file/
-        end
-      end
-
-      context 'and the configuration file has valid syntax' do
+      context 'when the configuration file has valid syntax' do
         it 'should read a new configuration object from the configuration file' do
+          section1 = {
+            'repository' => {
+              'name' => 'foo/dogs-repo'
+            },
+            'directory' => 'concepts'
+          }
+          expected_config_hash = {
+            'sections' => [section1],
+            'public_host' => 'http://example.com',
+            'pdf_index' => nil
+          }
+          config_hash_in_file = {
+            'sections' => [section1],
+            'public_host' => 'http://example.com',
+          }
           allow(loader).to receive(:load).with(path_to_config_file).
             and_return(config_hash_in_file)
           expect(Configuration).to receive(:new).with(logger, expected_config_hash)
           config_fetcher.fetch_config
-        end
-      end
-
-      context 'and the configuration file has invalid YAML syntax' do
-        before do
-          allow(loader).to receive(:load) { raise InvalidSyntaxError }
-        end
-
-        it 'raises an informative error' do
-          expect { config_fetcher.fetch_config }.to raise_error /There is a syntax error in your config file/
         end
       end
 
@@ -74,6 +49,24 @@ module Bookbinder
 
         expect(loader).not_to receive(:load)
         config_fetcher.fetch_config
+      end
+
+      context 'when the configuration file does not exist' do
+        it 'should raise an informative error' do
+          allow(loader).to receive(:load).with(path_to_config_file).
+            and_raise FileNotFoundError, "YAML"
+          expect { config_fetcher.fetch_config }.to raise_error /The configuration file specified does not exist. Please create a config YAML file/
+        end
+      end
+
+      context 'when the configuration file has invalid YAML syntax' do
+        before do
+          allow(loader).to receive(:load) { raise InvalidSyntaxError }
+        end
+
+        it 'raises an informative error' do
+          expect { config_fetcher.fetch_config }.to raise_error /There is a syntax error in your config file/
+        end
       end
     end
   end
