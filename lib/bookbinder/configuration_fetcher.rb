c60bf34b3922ba6d586216d0e60dcf7a18bc9f10
Andrew Bruce <andrew@camelpunch.com>
2015-02-09 16:38:31 +0000

    Remove cruft from config fetcher
    [#79052198]

--- lib/bookbinder/configuration_fetcher.rb
+++ lib/bookbinder/configuration_fetcher.rb
@@ -2,7 +2,6 @@ require_relative 'configuration'
 require_relative 'yaml_loader'
 
 module Bookbinder
-
   class ConfigurationFetcher
     def initialize(logger, configuration_validator, loader)
       @loader = loader
@@ -23,21 +22,12 @@ module Bookbinder
     attr_reader(:loader, :logger, :configuration_validator, :config, :config_file_path)
 
     def read_config_file
-      begin
-        config_hash = loader.load(config_file_path)
-      rescue FileNotFoundError => e
-        raise "The configuration file specified does not exist. Please create a config #{e} file at #{config_file_path} and try again."
-      rescue InvalidSyntaxError => e
-        raise "There is a syntax error in your config file: \n #{e}"
-      end
+      loader.load(config_file_path).merge('pdf_index' => nil)
 
-      if File.exists?('./pdf_index.yml')
-        config_hash['pdf_index'] = loader.load(config_file_path)
-      else
-        config_hash['pdf_index'] = nil
-      end
-
-      config_hash
+    rescue FileNotFoundError => e
+      raise "The configuration file specified does not exist. Please create a config #{e} file at #{config_file_path} and try again."
+    rescue InvalidSyntaxError => e
+      raise "There is a syntax error in your config file: \n #{e}"
     end
 
     def validate(config_hash)
