63a96076783ff2a22d28a7382adb0e892bfa7ca5
Andrew Bruce <andrew@camelpunch.com>
2015-02-13 18:20:50 +0000

    Stop stubbing stabilimentum
    It's a value, like a page object. It also wraps a 3rd party value, and
    we want to know when that API changes.
    
    [#88339564]

--- spec/lib/bookbinder/sieve_spec.rb
+++ spec/lib/bookbinder/sieve_spec.rb
@@ -2,44 +2,41 @@ require 'spec_helper'
 
 module Bookbinder
   describe Sieve do
+    def new_page(uri_str, options)
+      Spider::Stabilimentum.new(
+        Anemone::Page.new(
+          URI(uri_str),
+          { headers: {'content-type' => ['text/html']},
+            code: 200 }.
+          merge(options)
+        )
+      )
+    end
+
     describe '#links_from' do
       let(:sieve) { Sieve.new(domain: root_page_url) }
-      let(:page) { double(localized_links_in_stylesheets: []) }
       let(:root_page_url) { 'http://example.com/' }
 
       context 'when the page is found' do
-        before do
-          allow(page).to receive(:not_found?).and_return(false)
-          allow(page).to receive(:url).and_return(root_page_url)
-        end
-
         context 'and it has a valid html body' do
-          let(:frags) { [URI('#this-is-broken')] }
-
-          before do
-            allow(page).to receive(:fragment_identifiers).and_return(frags)
-            allow(page).to receive(:has_target_for?).with(frags.first).and_return(false)
-          end
-
           it 'returns the arrays passed to it, filled with links from the given page' do
-            broken_links, working_links = sieve.links_from(page, true)
+            broken_links, working_links = sieve.links_from(
+              new_page(root_page_url, body: '<a href="#this-is-broken"/>'),
+              first_pass = true
+            )
 
-            expect(broken_links).to eq(["/ => #{frags.pop}"])
+            expect(broken_links).to eq(["/ => #this-is-broken"])
             expect(working_links).to eq([root_page_url])
           end
         end
       end
 
       context 'when the page is not found' do
-        before do
-          allow(page).to receive(:not_found?).and_return(true)
-          allow(page).to receive(:url).and_return("#{root_page_url}fake")
-          allow(page).to receive(:referer).and_return(root_page_url)
-        end
-
         it 'returns the arrays passed to it, filled with links from the given page' do
-          broken_links, working_links = sieve.links_from(page, is_first_pass = true)
-
+          broken_links, working_links = sieve.links_from(
+            new_page("#{root_page_url}fake", code: 404, referer: root_page_url),
+            first_pass = true
+          )
           expect(broken_links).to eq(['/ => http://example.com/fake'])
           expect(working_links).to eq([])
         end
