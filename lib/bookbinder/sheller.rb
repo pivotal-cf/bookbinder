7aabd0e3cd1707e50d43c1d17776859db3c28d60
Andrew Bruce <andrew@camelpunch.com>
2015-03-17 19:42:08 +0000

    Sheller with send stderr to view_updater#error
    [#90425988]

--- lib/bookbinder/sheller.rb
+++ lib/bookbinder/sheller.rb
@@ -1,3 +1,5 @@
+require 'open3'
+
 module Bookbinder
   class Sheller
     ShelloutFailure = Class.new(RuntimeError)
@@ -7,11 +9,20 @@ module Bookbinder
     end
 
     def run_command(command)
-      IO.popen(command) do |stdout|
-        stdout.each { |line| view_updater.log line }
+      exit_status = nil
+      Open3.popen3(command) do |stdin, stdout, stderr, wait_thr|
+        stdout.each do |line|
+          view_updater.log(line)
+        end
+        stderr.each do |line|
+          view_updater.error(line)
+        end
+        exit_status = wait_thr.value
       end
 
-      raise ShelloutFailure.new "Shelling out failed." unless $?.success?
+      unless exit_status.success?
+        raise ShelloutFailure.new "Shelling out failed."
+      end
     end
 
     attr_reader :view_updater
--- spec/lib/bookbinder/sheller_spec.rb
+++ spec/lib/bookbinder/sheller_spec.rb
@@ -3,13 +3,22 @@ require_relative '../../../lib/bookbinder/sheller'
 module Bookbinder
   describe Sheller do
 
-    it 'prints to standard out' do
+    it 'redirects stdout to a log' do
       view_updater = double('view_updater', log: nil)
       sheller = Sheller.new(view_updater)
 
       expect(view_updater).to receive(:log).with("hello\n")
 
       sheller.run_command("echo 'hello'")
+    end
+
+    it 'redirects stderr to a log' do
+      view_updater = double('view_updater', log: nil)
+      sheller = Sheller.new(view_updater)
+
+      expect(view_updater).to receive(:error).with("hello\n")
+
+      sheller.run_command(">&2 echo hello")
     end
 
     context 'when the command exits with 1' do
