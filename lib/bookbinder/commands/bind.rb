31f600b34b5cea090926c146170c1feb2f0e6352
Andrew Bruce <andrew@camelpunch.com>
2015-04-20 15:45:27 +0100

    Remove Publisher class
    Inline methods into Bind

--- lib/bookbinder/commands/bind.rb
+++ lib/bookbinder/commands/bind.rb
@@ -74,8 +74,6 @@ module Bookbinder
         bind_source, *options = cli_arguments
         validate(bind_source, options)
 
-        publisher = Publisher.new(logger, sitemap_writer, static_site_generator, file_system_accessor)
-
         bind_config = config_factory.produce(bind_source)
         output_streams = Streams::SwitchableStdoutAndRedStderr.new(options)
 
@@ -124,7 +122,7 @@ module Bookbinder
 
         subnavs = (sections + gathered_dita_sections).map(&:subnav).reduce(&:merge)
 
-        success = publisher.publish(
+        success = publish(
           subnavs,
           {verbose: options.include?('--verbose')},
           output_locations,
@@ -132,56 +130,6 @@ module Bookbinder
         )
 
         success ? 0 : 1
-      end
-
-      class Publisher
-        def initialize(logger, sitemap_writer, static_site_generator, file_system_accessor)
-          @logger = logger
-          @sitemap_writer = sitemap_writer
-          @static_site_generator = static_site_generator
-          @file_system_accessor = file_system_accessor
-        end
-
-        def publish(subnavs, cli_options, output_paths, publish_config)
-          intermediate_directory = output_paths.output_dir
-          final_app_dir = output_paths.final_app_dir
-          master_dir = File.join intermediate_directory, 'master_middleman'
-          workspace_dir = File.join master_dir, 'source'
-          build_directory = File.join master_dir, 'build/.'
-          public_directory = File.join final_app_dir, 'public'
-
-          FileUtils.cp 'redirects.rb', final_app_dir if File.exists?('redirects.rb')
-
-          host_for_sitemap = publish_config.fetch(:host_for_sitemap)
-
-          generate_site(cli_options, output_paths, publish_config, master_dir, workspace_dir, subnavs, build_directory, public_directory)
-          result = generate_sitemap(host_for_sitemap)
-
-          @logger.log "Bookbinder bound your book into #{final_app_dir.to_s.green}"
-
-          !result.has_broken_links?
-        end
-
-        private
-
-        attr_reader :section_repository, :logger, :file_system_accessor, :sitemap_writer
-
-        def generate_sitemap(host_for_sitemap)
-          raise "Your public host must be a single String." unless host_for_sitemap.is_a?(String)
-          sitemap_writer.write(host_for_sitemap)
-        end
-
-        def generate_site(cli_options, output_paths, publish_config, middleman_dir, workspace_dir, subnavs, build_dir, public_dir)
-          @static_site_generator.run(middleman_dir,
-                                     workspace_dir,
-                                     publish_config.fetch(:template_variables, {}),
-                                     output_paths.local_repo_dir,
-                                     cli_options[:verbose],
-                                     subnavs,
-                                     publish_config[:host_for_sitemap],
-                                     publish_config[:archive_menu])
-          file_system_accessor.copy build_dir, public_dir
-        end
       end
 
       private
@@ -203,6 +151,43 @@ module Bookbinder
                   :command_creator,
                   :sheller,
                   :directory_preparer
+
+      def publish(subnavs, cli_options, output_paths, publish_config)
+        intermediate_directory = output_paths.output_dir
+        final_app_dir = output_paths.final_app_dir
+        master_dir = File.join intermediate_directory, 'master_middleman'
+        workspace_dir = File.join master_dir, 'source'
+        build_directory = File.join master_dir, 'build/.'
+        public_directory = File.join final_app_dir, 'public'
+
+        FileUtils.cp 'redirects.rb', final_app_dir if File.exists?('redirects.rb')
+
+        host_for_sitemap = publish_config.fetch(:host_for_sitemap)
+
+        generate_site(cli_options, output_paths, publish_config, master_dir, workspace_dir, subnavs, build_directory, public_directory)
+        result = generate_sitemap(host_for_sitemap)
+
+        @logger.log "Bookbinder bound your book into #{final_app_dir.to_s.green}"
+
+        !result.has_broken_links?
+      end
+
+      def generate_sitemap(host_for_sitemap)
+        raise "Your public host must be a single String." unless host_for_sitemap.is_a?(String)
+        sitemap_writer.write(host_for_sitemap)
+      end
+
+      def generate_site(cli_options, output_paths, publish_config, middleman_dir, workspace_dir, subnavs, build_dir, public_dir)
+        @static_site_generator.run(middleman_dir,
+                                   workspace_dir,
+                                   publish_config.fetch(:template_variables, {}),
+                                   output_paths.local_repo_dir,
+                                   cli_options[:verbose],
+                                   subnavs,
+                                   publish_config[:host_for_sitemap],
+                                   publish_config[:archive_menu])
+        file_system_accessor.copy build_dir, public_dir
+      end
 
       def generate_local_repo_dir(context_dir, bind_source)
         File.expand_path('..', context_dir) if bind_source == 'local'
