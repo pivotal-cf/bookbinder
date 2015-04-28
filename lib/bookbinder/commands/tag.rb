7ce8639d2bced277c41861d0c13b726a3db387f4
Andrew Bruce <andrew@camelpunch.com>
2015-04-28 15:20:44 +0100

    Tag command no longer inherits
    We need control of the constructor, and the inheritance doesn't provide
    much anyway.
    
    Signed-off-by: Andrew Bruce <andrew@camelpunch.com>

--- lib/bookbinder/commands/tag.rb
+++ lib/bookbinder/commands/tag.rb
@@ -1,13 +1,17 @@
 require_relative '../book'
-require_relative '../errors/cli_error'
 require_relative '../deprecated_logger'
-require_relative 'bookbinder_command'
+require_relative '../errors/cli_error'
 require_relative 'naming'
 
 module Bookbinder
   module Commands
-    class Tag < BookbinderCommand
+    class Tag
       include Commands::Naming
+
+      def initialize(logger, configuration_fetcher)
+        @logger = logger
+        @configuration_fetcher = configuration_fetcher
+      end
 
       def usage
         ["tag <git tag>", "Apply the specified <git tag> to your book and all sections of your book"]
@@ -24,6 +28,14 @@ module Bookbinder
         @logger.log 'Success!'.green
         @logger.log " #{book.full_name.yellow} and its sections were tagged with #{tag.blue}"
         0
+      end
+
+      private
+
+      attr_reader :configuration_fetcher
+
+      def config
+        @config ||= configuration_fetcher.fetch_config
       end
     end
   end
