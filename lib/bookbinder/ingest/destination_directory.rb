c6098e089545243f229e22564438fd601fc72809
Elena Sharma <esharma@pivotal.io>
2015-05-20 14:28:53 -0700

    Integer dest dir converted to a string.
    [Fixes #94595504]
    
    Signed-off-by: Frank Kotsianas <frankkotsianas@gmail.com>

--- lib/bookbinder/ingest/destination_directory.rb
+++ lib/bookbinder/ingest/destination_directory.rb
@@ -3,7 +3,7 @@ module Bookbinder
     DestinationDirectory = Struct.new(:full_repo_name, :desired_destination_dir_name) do
       def to_str
         if desired_destination_dir_name
-          desired_destination_dir_name
+          desired_destination_dir_name.to_s
         elsif full_repo_name
           full_repo_name.split('/').last
         else
