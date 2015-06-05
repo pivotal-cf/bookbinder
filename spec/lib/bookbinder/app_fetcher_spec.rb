59873dfa1b2851f5a5c8bf917e0ad8196bae0e14
Andrew Bruce <andrew@camelpunch.com>
2015-06-05 20:01:05 +0100

    Moisturize app_fetcher_spec
    - Un-let routes_output
    - Add a pending example for without-space cf routes output
    
    [#96245104]

--- spec/lib/bookbinder/app_fetcher_spec.rb
+++ spec/lib/bookbinder/app_fetcher_spec.rb
@@ -3,9 +3,14 @@ require_relative '../../../lib/bookbinder/app_fetcher'
 module Bookbinder
   describe AppFetcher do
     describe 'retrieving apps' do
-      let(:routes_output) do
-        eol_space = ' '
-        <<OUTPUT
+      let(:routes_to_search) { [['cfapps.io', 'docs']] }
+      let(:cf_command_runner) { double 'cf_command_runner' }
+      let(:eol_space) { ' ' }
+
+      context 'with a space column in the cf routes output' do
+        let(:subject) { AppFetcher.new(routes_to_search, cf_command_runner) }
+        it 'returns the correct app' do
+          allow(cf_command_runner).to receive(:cf_routes_output).and_return(<<OUTPUT)
 Getting routes as cfaccounts+cfdocs@pivotallabs.com ...
 
 space       host                    domain                apps
@@ -18,66 +23,59 @@ my-space    docs-testmisleading     cfapps.io
 my-space    docs-test               cfapps.io             docs-green,docs-blue #{eol_space}
 my-space    more-cat-pictures       cfapps.io             many-cats, too-many-cats #{eol_space}
 OUTPUT
+          expect(subject.fetch_current_app).to eq(BlueGreenApp.new('docs-green'))
+        end
       end
 
-      let(:routes_to_search) { [['cfapps.io', 'docs']] }
-      let(:cf_command_runner) { double 'cf_command_runner' }
-      let(:subject) { AppFetcher.new(routes_to_search, cf_command_runner) }
-
-      before do
-        allow(cf_command_runner).to receive(:cf_routes_output).and_return(routes_output)
-      end
-
-      it 'returns the correct app' do
-        expect(subject.fetch_current_app).to eq(BlueGreenApp.new('docs-green'))
-      end
-
-      it 'only queries the API once' do
-        expect(cf_command_runner).to receive(:cf_routes_output).once
-        subject.fetch_current_app
+      context 'without a space column in the cf routes output' do
+        xit 'returns the correct app' do
+          expect(subject.fetch_current_app).to eq(BlueGreenApp.new('docs-green'))
+        end
       end
 
       context 'when there are no apps' do
-        let(:routes_output) do
-          eol_space = ' '
-          <<OUTPUT
+        let(:subject) { AppFetcher.new(routes_to_search, cf_command_runner) }
+        it 'returns nil' do
+          allow(cf_command_runner).to receive(:cf_routes_output).and_return(<<OUTPUT)
   Getting routes as cfaccounts+cfdocs@pivotallabs.com ...
 
 space         host                    domain                apps
 cool-space    docs                    cfapps.io             #{eol_space}
 OUTPUT
-        end
-
-        it 'returns nil' do
           expect(subject.fetch_current_app).to be_nil
         end
       end
 
       context 'when the host is not found' do
-        let(:routes_output) do
-          eol_space = ' '
-          <<OUTPUT
+        let(:subject) { AppFetcher.new(routes_to_search, cf_command_runner) }
+        it 'returns nil' do
+          allow(cf_command_runner).to receive(:cf_routes_output).and_return(<<OUTPUT)
   Getting routes as cfaccounts+cfdocs@pivotallabs.com ...
 
 space           host                    domain                apps
 cool-space      foo                     cfapps.io             fantastic-app #{eol_space}
 OUTPUT
-        end
-
-        it 'returns nil' do
           expect(subject.fetch_current_app).to be_nil
         end
       end
 
       context "when there are spaces in between app names" do
+        let(:subject) { AppFetcher.new(routes_to_search, cf_command_runner) }
         let(:routes_to_search) { [['cfapps.io', 'more-cat-pictures']] }
 
         it "returns app names with stripped spaces" do
+          allow(cf_command_runner).to receive(:cf_routes_output).and_return(<<OUTPUT)
+Getting routes as cfaccounts+cfdocs@pivotallabs.com ...
+
+space       host                    domain                apps
+my-space    more-cat-pictures       cfapps.io             many-cats, too-many-cats #{eol_space}
+OUTPUT
           expect(subject.fetch_current_app).to eq(BlueGreenApp.new('many-cats'))
         end
       end
 
       context 'when a route in the creds is not yet mapped in the app' do
+        let(:subject) { AppFetcher.new(routes_to_search, cf_command_runner) }
         let(:config_hash) do
           {
               'staging_host' => {
@@ -87,6 +85,13 @@ OUTPUT
         end
 
         it "returns the apps for the mapped routes" do
+          allow(cf_command_runner).to receive(:cf_routes_output).and_return(<<OUTPUT)
+Getting routes as cfaccounts+cfdocs@pivotallabs.com ...
+
+space       host                    domain                apps
+my-space    docs                    cfapps.io             docs-green #{eol_space}
+my-space    docs-test               cfapps.io             docs-green,docs-blue #{eol_space}
+OUTPUT
           expect(subject.fetch_current_app).to eq(BlueGreenApp.new('docs-green'))
         end
       end
