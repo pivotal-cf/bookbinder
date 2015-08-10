3062c580985121e6a2a92e53a0267c32458524f4
Andrew Bruce <andrew@camelpunch.com>
2015-08-10 15:45:56 +0100

    Refactor and decruft Bind a bit
    [#99847340]

--- lib/bookbinder/commands/bind/bind_options.rb
+++ lib/bookbinder/commands/bind/bind_options.rb
@@ -1,5 +1,4 @@
 require_relative '../../sheller'
-require_relative '../../streams/colorized_stream'
 
 module Bookbinder
   module Commands
@@ -14,14 +13,30 @@ module Bookbinder
           raise CliError::InvalidArguments unless arguments_are_valid?
         end
 
+        def bind_source
+          opts.first
+        end
+
+        def local_repo_dir
+          File.expand_path('..') if bind_source == 'local'
+        end
+
+        def options
+          opts[1..-1]
+        end
+
         def ref_override
-          'master' if opts.include?('--ignore-section-refs')
+          'master' if options.include?('--ignore-section-refs')
         end
 
         def streams
           base_streams.merge(
-            out: opts.include?('--verbose') ? base_streams[:out] : Sheller::DevNull.new,
+            out: verbose? ? base_streams[:out] : Sheller::DevNull.new,
           )
+        end
+
+        def verbose?
+          options.include?('--verbose')
         end
 
         private
@@ -29,20 +44,15 @@ module Bookbinder
         attr_accessor :base_streams, :opts
 
         def arguments_are_valid?
-          valid_options = %w(--verbose --ignore-section-refs --dita-flags).to_set
-          %w(local remote github).include?(bind_source) && flag_names.to_set.subset?(valid_options)
+          %w(local remote github).include?(bind_source) && flag_names.subset?(valid_options)
+        end
+
+        def valid_options
+          %w(--verbose --ignore-section-refs --dita-flags).to_set
         end
 
         def flag_names
-          options.map {|o| o.split('=').first}
-        end
-
-        def bind_source
-          opts.first
-        end
-
-        def options
-          opts[1..-1]
+          options.map {|o| o.split('=').first}.to_set
         end
       end
     end
