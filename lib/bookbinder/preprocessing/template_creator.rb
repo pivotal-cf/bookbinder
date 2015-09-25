5e5b7b0fcd236f2da7e4ff07498a1837fd2f61b7
Elena Sharma <esharma@pivotal.io>
2015-09-25 14:22:45 -0700

    Subnav's .erb files generated from template
    [#103820272]
    
    Signed-off-by: Christa Hartsock <chartsock@pivotal.io>

---
+++ lib/bookbinder/preprocessing/template_creator.rb
@@ -0,0 +1,41 @@
+module Bookbinder
+  module Preprocessing
+    class TemplateCreator
+      def initialize(fs, output_locations, html_doc_manipulator)
+        @fs = fs
+        @output_locations = output_locations
+        @html_doc_manipulator = html_doc_manipulator
+      end
+
+      def create(props_filename, subnav_config)
+        template_content = fs.read(template_path)
+        nav_content = html_doc_manipulator.set_attribute(document: template_content,
+                                                         selector: 'div.nav-content',
+                                                         attribute: 'data-props-location',
+                                                         value: props_filename)
+
+        fs.write(text: nav_content, to: subnav_destination(subnav_config.name))
+      end
+
+      attr_reader :fs, :output_locations, :html_doc_manipulator
+
+      private
+
+      def subnavs_path
+        output_locations.subnavs_for_layout_dir
+      end
+
+      def filename(name)
+        "#{name}_subnav.erb"
+      end
+
+      def template_path
+        subnavs_path.join('subnav_template.erb')
+      end
+
+      def subnav_destination(name)
+        subnavs_path.join(filename(name))
+      end
+    end
+  end
+end
---
+++ spec/lib/bookbinder/preprocessing/template_creator_spec.rb
@@ -0,0 +1,34 @@
+require_relative '../../../../lib/bookbinder/html_document_manipulator'
+require_relative '../../../../lib/bookbinder/local_filesystem_accessor'
+require_relative '../../../../lib/bookbinder/preprocessing/template_creator'
+require_relative '../../../../lib/bookbinder/values/output_locations'
+require_relative '../../../../lib/bookbinder/config/subnav_config'
+
+module Bookbinder
+  module Preprocessing
+    describe TemplateCreator do
+      it 'writes the template to subnavs directory' do
+        output_locations = OutputLocations.new(context_dir: '.')
+        subnav_config = Config::SubnavConfig.new({'name' => 'best'})
+        props_filename = 'props.json'
+        template_content = 'our template'
+        generated_content = 'generated template'
+
+        fs = instance_double('Bookbinder::LocalFilesystemAccessor')
+        html_doc_manipulator = instance_double('Bookbinder::HtmlDocumentManipulator')
+
+        subnavs_dir = output_locations.subnavs_for_layout_dir
+
+        expect(fs).to receive(:read).with(subnavs_dir.join('subnav_template.erb')) { template_content }.ordered
+        expect(html_doc_manipulator).to receive(:set_attribute).with(document: template_content,
+                                                                     selector: 'div.nav-content',
+                                                                     attribute: 'data-props-location',
+                                                                     value: props_filename) { generated_content}.ordered
+
+        expect(fs).to receive(:write).with(text: generated_content, to: subnavs_dir.join('best_subnav.erb')).ordered
+
+        TemplateCreator.new(fs, output_locations, html_doc_manipulator).create(props_filename, subnav_config)
+      end
+    end
+  end
+end
