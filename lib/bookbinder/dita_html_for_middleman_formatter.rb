aae20a9ee966e716c914bdfabdced16afa2bb492
Christa Hartsock <chartsock@pivotal.io>
2015-10-26 17:33:35 -0700

    Add ERB support for DITA
    [Finishes #106710770]
    
    Signed-off-by: Elena Sharma <esharma@pivotal.io>

--- lib/bookbinder/dita_html_for_middleman_formatter.rb
+++ lib/bookbinder/dita_html_for_middleman_formatter.rb
@@ -10,17 +10,20 @@ module Bookbinder
       all_files_with_ext = file_system_accessor.find_files_with_ext('html', src)
 
       all_files_with_ext.map do |filepath|
-        file_text = file_system_accessor.read filepath
+        file_text = file_system_accessor.read(filepath)
+
         file_title_text = html_document_manipulator.read_html_in_tag(document: file_text,
                                                                      tag: 'title')
 
         file_body_text = html_document_manipulator.read_html_in_tag(document: file_text,
                                                                     tag: 'body')
 
+        cleansed_body_text = revert_to_erb(file_body_text)
+
         relative_path_to_file = file_system_accessor.relative_path_from(src, filepath)
         new_filepath = File.join dest, "#{relative_path_to_file}.erb"
 
-        output_text = frontmatter(file_title_text) + file_body_text
+        output_text = frontmatter(file_title_text) + cleansed_body_text
 
         file_system_accessor.write(to: new_filepath, text: output_text)
       end
@@ -34,6 +37,9 @@ module Bookbinder
       sanitized_title = title.gsub('"', '\"')
       "---\ntitle: \"#{sanitized_title}\"\ndita: true\n---\n"
     end
-  end
 
+    def revert_to_erb(text)
+      text.gsub('&lt;%', '<%').gsub('%&gt;', '%>')
+    end
+  end
 end
--- spec/lib/bookbinder/dita_html_for_middleman_formatter_spec.rb
+++ spec/lib/bookbinder/dita_html_for_middleman_formatter_spec.rb
@@ -4,18 +4,19 @@ require_relative '../../../lib/bookbinder/html_document_manipulator'
 
 module Bookbinder
   describe DitaHtmlForMiddlemanFormatter do
-    let(:fs) { instance_double('Bookbinder::LocalFilesystemAccessor', find_files_with_ext: ['thing.html'], read: 'some text') }
+    let(:fs) { instance_double('Bookbinder::LocalFilesystemAccessor', find_files_with_ext: ['thing.html'], read: html_text) }
     let(:html_doc_manipulator) { instance_double('Bookbinder::HtmlDocumentManipulator') }
+    let(:html_text) { 'some text' }
 
     describe 'formatting HTML from DITA' do
       it 'creates a properly formatted ERB file with the same name for every HTML file in the directory' do
-        frontmatter = "---\ntitle: \"title \\\"text\\\"\"\ndita: true\n---\n"
-        expected_html = frontmatter + 'body text'
-
         allow(html_doc_manipulator).to receive(:read_html_in_tag).with(document: 'some text', tag: 'title') { 'title "text"' }
-        allow(html_doc_manipulator).to receive(:read_html_in_tag).with(document: 'some text', tag: 'body') { 'body text' }
+        allow(html_doc_manipulator).to receive(:read_html_in_tag).with(document: 'some text', tag: 'body') { 'body text &lt;% tag %&gt;' }
 
         allow(fs).to receive(:relative_path_from).with('src/folder', 'thing.html') { 'rel/path/file.html' }
+
+        frontmatter = "---\ntitle: \"title \\\"text\\\"\"\ndita: true\n---\n"
+        expected_html = frontmatter + 'body text <% tag %>'
 
         expect(fs).to receive(:write).with(to: 'dest/folder/rel/path/file.html.erb', text: expected_html)
 
