06a6bffd7b853349b826f602ce627accc9d6a04f
Alpha Chen <achen@pivotal.io>
2016-02-25 09:50:53 -0800

    Refactor getting subnav nodes
    [#114419637]
    
    Signed-off-by: Michael Trestman <mtrestman@pivotal.io>

--- lib/bookbinder/subnav/json_from_markdown_toc.rb
+++ lib/bookbinder/subnav/json_from_markdown_toc.rb
@@ -51,36 +51,31 @@ Source file: #{@parsed_files[current_expanded_href]}
         toc_md = fs.read(full_source)
         base_node = get_html(toc_md).css('html')
 
-        nav_items(base_node).flat_map do |element|
-          unless frontmatter_header?(element)
-            a = element.at_css('a')
-            next if a.nil?
+        nav_items(base_node).map do |element|
+          a = element.at_css('a')
+          href = a['href']
 
-            href = a['href']
-            next if href.nil?
-
-            next_expanded_href = current_expanded_path.dirname.join(href).to_s
-            raise SubnavDuplicateLinkError.new(<<-ERROR) if @parsed_files.has_key?(next_expanded_href)
- )
+          next_expanded_href = current_expanded_path.dirname.join(href).to_s
+          raise SubnavDuplicateLinkError.new(<<-ERROR) if @parsed_files.has_key?(next_expanded_href)
+)
 Duplicate link found in subnav for product_id: #{config.id}
 
 Link: #{next_expanded_href}
 Original file: #{@parsed_files[next_expanded_href]}
 Current file: #{full_source}
-            ERROR
-            @parsed_files[next_expanded_href] = full_source
+          ERROR
+          @parsed_files[next_expanded_href] = full_source
 
-            nested_urls_and_texts = gather_urls_and_texts(next_expanded_href )
-            nested_links = {}
-            nested_links.merge!(nestedLinks: nested_urls_and_texts) unless nested_urls_and_texts.empty?
+          nested_urls_and_texts = gather_urls_and_texts(next_expanded_href )
+          nested_links = {}
+          nested_links.merge!(nestedLinks: nested_urls_and_texts) unless nested_urls_and_texts.empty?
 
-            {url: '/' + next_expanded_href , text: element.inner_text}.merge(nested_links)
-          end
-        end.compact
+          {url: '/' + next_expanded_href , text: element.inner_text}.merge(nested_links)
+        end
       end
 
       def nav_items(base_node)
-        base_node.css('h2') - base_node.css(*exclusions)
+        base_node.xpath('/html/body/h2[a[@href]]') # - base_node.css(*exclusions)
       end
 
       def exclusions
