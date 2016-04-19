e7a3acbfcc9370df68520b720a6f2526770f291f
Gregg Van Hove <gvanhove@pivotal.io>
2016-04-19 11:32:39 -0700

    Let's try showing error messages for now
    [#117668213]
    
    Signed-off-by: Sarah McAlear <smcalear@pivotal.io>

--- template_app/lib/search.rb
+++ template_app/lib/search.rb
@@ -8,18 +8,26 @@ module Bookbinder
       client = Elasticsearch::Client.new log: true, url: JSON.parse(ENV['VCAP_SERVICES'])['searchly'][0]['credentials']['uri']
       query = env['QUERY_STRING'].split('&').detect { |s| s =~ /\Aq=/ }.split('=').last
       query_options = YAML.load_file(File.expand_path('../../search.yml', __FILE__))
-      query_options['query_string']['query'] = query
+      query_options['query']['query_string']['query'] = query
       results = client.search index: 'searching', body: query_options
 
       erb = ERB.new(File.read(File.expand_path('../../search-results.html.erb', __FILE__)))
 
-      search_data = {
-        query: query,
-        total_results: results['hits']['total'],
-        search_results: results['hits']['hits']
-      }
+      search_data = Result.new(
+        query,
+        results['hits']['total'],
+        results['hits']['hits']
+      )
 
-      [200, {'Content-Type' => 'application/json'}, [erb.result(search_data)]]
+      [200, {'Content-Type' => 'text/html'}, [search_data.render(erb)]]
+    rescue Exception => e
+      [500, {'Content-Type' => 'text/plain'}, [e.message + "\n" + e.backtrace.join("\n")]]
+    end
+
+    class Result < Struct.new(:query, :result_count, :search_results)
+      def render(erb)
+        erb.result(binding)
+      end
     end
   end
 end
