eb603992f64a5ea97f80c577f76b920f947d82fb
Sarah McAlear <smcalear@pivotal.io>
2016-04-19 12:04:27 -0700

    don't log elastic search requests
    Signed-off-by: Gregg Van Hove <gvanhove@pivotal.io>

--- template_app/lib/search.rb
+++ template_app/lib/search.rb
@@ -5,7 +5,7 @@ require 'erb'
 module Bookbinder
   module Search
     def self.call(env)
-      client = Elasticsearch::Client.new log: true, url: JSON.parse(ENV['VCAP_SERVICES'])['searchly'][0]['credentials']['uri']
+      client = Elasticsearch::Client.new url: JSON.parse(ENV['VCAP_SERVICES'])['searchly'][0]['credentials']['uri']
       query = env['QUERY_STRING'].split('&').detect { |s| s =~ /\Aq=/ }.split('=').last
       query_options = YAML.load_file(File.expand_path('../../search.yml', __FILE__))
       query_options['query']['query_string']['query'] = query
