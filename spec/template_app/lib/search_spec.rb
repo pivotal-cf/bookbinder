b2b1f0ddc2fab01c4538857ef773664e8329ff72
Gregg Van Hove <gvanhove@pivotal.io>
2016-04-20 10:10:29 -0700

    Empty search results
    [#113281377]
    
    Signed-off-by: Sarah McAlear <smcalear@pivotal.io>

---
+++ spec/template_app/lib/search_spec.rb
@@ -0,0 +1,88 @@
+require_relative '../../../template_app/lib/search'
+
+describe Bookbinder::Search do
+  before do
+    allow(Bookbinder::Search).to receive(:search_client) { mock_searcher }
+    allow(Bookbinder::Search::Result).to receive(:layout_content) { '<%= yield %>' }
+  end
+
+  let(:mock_searcher) { double(:search) }
+
+  it 'shows search results' do
+    allow(mock_searcher).to receive(:search) do
+      {
+        'hits' => {
+          'total' => 3,
+          'hits' => [
+            {
+              '_source' => {
+                'url' => 'hi.html',
+                'title' => 'Hi'
+              },
+              'highlight' => {
+                'text' => [' Im a highlight ']
+              }
+            },
+            {
+              '_source' => {
+                'url' => 'bye.html',
+                'title' => 'Bye'
+              },
+              'highlight' => {
+                'text' => [' Im bye highlight ']
+              }
+            },
+            {
+              '_source' => {
+                'url' => 'another.html',
+                'title' => 'Another'
+              },
+              'highlight' => {
+                'text' => [' Im another highlight ']
+              }
+            },
+          ]
+        }
+      }
+    end
+
+    resulting_page = Bookbinder::Search.call({ 'QUERY_STRING' => 'foo=bar&q=search&baz=quux' })
+
+    expect(resulting_page.last.first).to include('<a href="hi.html">Hi</a>')
+    expect(resulting_page.last.first).to include('<p>Im a highlight</p>')
+    expect(resulting_page.last.first).to include('Bye')
+    expect(resulting_page.last.first).to include('another')
+
+    expect(mock_searcher).to have_received(:search).with({index: 'searching', body: {
+      'query' => { 'query_string' => { 'query' => 'search', 'default_field' => 'text' }},
+      '_source' => [ 'url', 'title' ], 'highlight' => { 'fields' => { 'text' => { 'type' => 'plain' }}}
+    }})
+  end
+
+  it 'should show no results if no results were found' do
+    allow(mock_searcher).to receive(:search) do
+      {
+        'hits' => {
+          'total' => 0,
+          'hits' => []
+        }
+      }
+    end
+
+    resulting_page = Bookbinder::Search.call({ 'QUERY_STRING' => 'foo=nothinghere&q=search&baz=nothinghere' })
+
+    expect(resulting_page.last.first).to include('No Results')
+  end
+
+  it 'should show no results for an empty search query' do
+    resulting_page = Bookbinder::Search.call({ 'QUERY_STRING' => 'q=' })
+
+    expect(resulting_page.last.first).to include('No Results')
+  end
+
+  it 'should show no results for a request with no query' do
+    resulting_page = Bookbinder::Search.call({ 'QUERY_STRING' => 'foo=bar' })
+
+    expect(resulting_page.last.first).to include('No Results')
+  end
+end
--- template_app/lib/search.rb
+++ template_app/lib/search.rb
@@ -5,23 +5,44 @@ require 'erb'
 module Bookbinder
   module Search
     def self.call(env)
-      client = Elasticsearch::Client.new url: JSON.parse(ENV['VCAP_SERVICES'])['searchly'][0]['credentials']['uri']
-      query = env['QUERY_STRING'].split('&').detect { |s| s =~ /\Aq=/ }.split('=').last
+      search_data = search(get_search_query(env['QUERY_STRING']))
+
+      erb = ERB.new(File.read(File.expand_path('../../search-results.html.erb', __FILE__)))
+
+      [200, {'Content-Type' => 'text/html'}, [search_data.render(erb)]]
+    rescue Exception => e
+      puts e.message
+      puts e.backtrace.join("\n")
+
+      [500, {'Content-Type' => 'text/plain'}, ['An error occurred']]
+    end
+
+    def self.search(query)
+      return Result.new(query, 0, []) if query == ''
+
+      client = search_client
+
       query_options = YAML.load_file(File.expand_path('../../search.yml', __FILE__))
       query_options['query']['query_string']['query'] = query
       results = client.search index: 'searching', body: query_options
 
-      erb = ERB.new(File.read(File.expand_path('../../search-results.html.erb', __FILE__)))
-
-      search_data = Result.new(
+      Result.new(
         query,
         results['hits']['total'],
         results['hits']['hits']
       )
+    end
 
-      [200, {'Content-Type' => 'text/html'}, [search_data.render(erb)]]
-    rescue Exception => e
-      [500, {'Content-Type' => 'text/plain'}, [e.message + "\n" + e.backtrace.join("\n")]]
+    def self.get_search_query(query_string)
+      q_param = query_string.split('&').detect { |s| s =~ /\Aq=/ }
+
+      return '' if q_param.nil?
+
+      q_param.split('=')[1] || ''
+    end
+
+    def self.search_client
+      Elasticsearch::Client.new url: JSON.parse(ENV['VCAP_SERVICES'])['searchly'][0]['credentials']['uri']
     end
 
     Result = Struct.new(:query, :result_count, :search_results) do
@@ -33,7 +54,11 @@ module Bookbinder
       end
 
       def render_layout
-        ERB.new(File.read(File.expand_path('../../public/search.html', __FILE__))).result(binding)
+        ERB.new(self.class.layout_content).result(binding)
+      end
+
+      def self.layout_content
+        File.read(File.expand_path('../../public/search.html', __FILE__))
       end
     end
   end
