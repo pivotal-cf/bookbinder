f438583d538959244fa5ab775b26b06cffb27473
Gregg Van Hove <gvanhove@pivotal.io>
2016-06-09 12:18:05 -0700

    Show product information in search results if available
    [finish #121086871]

--- template_app/lib/search/hit.rb
+++ template_app/lib/search/hit.rb
@@ -5,11 +5,13 @@ module Bookbinder
         source_fields = attrs.fetch('_source')
         @title = source_fields.fetch('title').sub(/\A(.+)\|.*\z/, '\\1').strip
         @url = source_fields.fetch('url')
+        @product_name = source_fields['product_name']
+        @product_version = source_fields['product_version']
 
         @text = attrs.fetch('highlight').fetch('text').first.strip
       end
 
-      attr_reader :title, :url, :text
+      attr_reader :title, :url, :text, :product_name, :product_version
     end
   end
 end
